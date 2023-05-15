a605622bc0006d350d31e55c8a361a4a212450b7
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-05-15 09:58:35 -0400

    método post back-end

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,21 @@ app.get('/', async (req:Request, res:Response) => {
     const result = await banco.query(consulta);
     res.send(result[0]);
 });
+app.post('/', async (req:Request, res:Response) => {
+    //Conexao com o banco.
+    const banco = await mysql2.createConnection({
+        host:"localhost",
+        user:"test",
+        password:"test",
+        database:"test"
+    });
+    const consulta = 
+        "INSERT INTO pessoas VALUES ('5','Valerim','32')";
+    const result = await banco.query(consulta);
+    banco.end();
+    res.send(result[0]);
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
